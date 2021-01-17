5921b93e3e9de0ec0f9efde93926b218a7242ada
BenZalatan <bzalatan1@gmail.com>
2021-01-16 21:15:26 -0500

    ui improvements

---
+++ GrainBound_Installer/AALabel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace GrainBound_Installer
+{
+    public class AALabel : Label
+    {
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+            e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            e.Graphics.DrawString(Text, Font, new SolidBrush(ForeColor), 0, 0);
+        }
+    }
+}
--- GrainBound_Installer/MainForm.Designer.cs
+++ GrainBound_Installer/MainForm.Designer.cs
@@ -21,6 +21,19 @@ namespace GrainBound_Installer
             base.Dispose(disposing);
         }
 
+        /*
+         ************** FOR CUSTOM FONTS
+         * 
+        private System.Drawing.Text.PrivateFontCollection pfc = new System.Drawing.Text.PrivateFontCollection();
+        private void initCustomFont()
+        {
+            byte[] fontData = Properties.Resources.Avenir_LT_85_Heavy;
+            System.IntPtr fontPtr = System.Runtime.InteropServices.Marshal.AllocCoTaskMem(fontData.Length);
+            System.Runtime.InteropServices.Marshal.Copy(fontData, 0, fontPtr, fontData.Length);
+            pfc.AddMemoryFont(fontPtr, fontData.Length);
+        }
+        */
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -35,25 +48,25 @@ namespace GrainBound_Installer
             this.pgbProgress = new System.Windows.Forms.ProgressBar();
             this.lblStatus = new System.Windows.Forms.Label();
             this.pboxLogo = new System.Windows.Forms.PictureBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.lblEmail = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
-            this.label8 = new System.Windows.Forms.Label();
-            this.label13 = new System.Windows.Forms.Label();
-            this.label14 = new System.Windows.Forms.Label();
             this.tboxLocation = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.cboxDesktopShortcut = new System.Windows.Forms.CheckBox();
             this.btnInstallLocation = new System.Windows.Forms.Button();
+            this.label1 = new GrainBound_Installer.AALabel();
+            this.label14 = new GrainBound_Installer.AALabel();
+            this.label13 = new GrainBound_Installer.AALabel();
+            this.label8 = new GrainBound_Installer.AALabel();
+            this.label7 = new GrainBound_Installer.AALabel();
+            this.label6 = new GrainBound_Installer.AALabel();
+            this.lblEmail = new GrainBound_Installer.AALabel();
+            this.label4 = new GrainBound_Installer.AALabel();
+            this.label3 = new GrainBound_Installer.AALabel();
+            this.label2 = new GrainBound_Installer.AALabel();
             ((System.ComponentModel.ISupportInitialize)(this.pboxLogo)).BeginInit();
             this.SuspendLayout();
             // 
             // btnInstall
             // 
-            this.btnInstall.Location = new System.Drawing.Point(464, 278);
+            this.btnInstall.Location = new System.Drawing.Point(466, 298);
             this.btnInstall.Margin = new System.Windows.Forms.Padding(2);
             this.btnInstall.Name = "btnInstall";
             this.btnInstall.Size = new System.Drawing.Size(93, 37);
@@ -64,7 +77,7 @@ namespace GrainBound_Installer
             // 
             // btnCancel
             // 
-            this.btnCancel.Location = new System.Drawing.Point(562, 278);
+            this.btnCancel.Location = new System.Drawing.Point(564, 298);
             this.btnCancel.Margin = new System.Windows.Forms.Padding(2);
             this.btnCancel.Name = "btnCancel";
             this.btnCancel.Size = new System.Drawing.Size(93, 37);
@@ -75,7 +88,7 @@ namespace GrainBound_Installer
             // 
             // pgbProgress
             // 
-            this.pgbProgress.Location = new System.Drawing.Point(12, 278);
+            this.pgbProgress.Location = new System.Drawing.Point(14, 298);
             this.pgbProgress.Margin = new System.Windows.Forms.Padding(2);
             this.pgbProgress.Name = "pgbProgress";
             this.pgbProgress.Size = new System.Drawing.Size(448, 36);
@@ -84,7 +97,7 @@ namespace GrainBound_Installer
             // lblStatus
             // 
             this.lblStatus.AutoSize = true;
-            this.lblStatus.Location = new System.Drawing.Point(9, 262);
+            this.lblStatus.Location = new System.Drawing.Point(11, 282);
             this.lblStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblStatus.Name = "lblStatus";
             this.lblStatus.Size = new System.Drawing.Size(40, 13);
@@ -105,16 +118,118 @@ namespace GrainBound_Installer
             this.pboxLogo.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pboxLogo_MouseDown);
             this.pboxLogo.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pboxLogo_MouseMove);
             // 
-            // label2
+            // tboxLocation
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(6, 9);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(296, 31);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Early Access Preview";
+            this.tboxLocation.Location = new System.Drawing.Point(14, 259);
+            this.tboxLocation.Name = "tboxLocation";
+            this.tboxLocation.Size = new System.Drawing.Size(448, 20);
+            this.tboxLocation.TabIndex = 18;
+            // 
+            // cboxDesktopShortcut
+            // 
+            this.cboxDesktopShortcut.AutoSize = true;
+            this.cboxDesktopShortcut.Checked = true;
+            this.cboxDesktopShortcut.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.cboxDesktopShortcut.Location = new System.Drawing.Point(514, 261);
+            this.cboxDesktopShortcut.Name = "cboxDesktopShortcut";
+            this.cboxDesktopShortcut.Size = new System.Drawing.Size(143, 17);
+            this.cboxDesktopShortcut.TabIndex = 20;
+            this.cboxDesktopShortcut.Text = "Create Desktop Shortcut";
+            this.cboxDesktopShortcut.UseVisualStyleBackColor = true;
+            // 
+            // btnInstallLocation
+            // 
+            this.btnInstallLocation.Location = new System.Drawing.Point(466, 257);
+            this.btnInstallLocation.Name = "btnInstallLocation";
+            this.btnInstallLocation.Size = new System.Drawing.Size(42, 23);
+            this.btnInstallLocation.TabIndex = 21;
+            this.btnInstallLocation.Text = "...";
+            this.btnInstallLocation.UseVisualStyleBackColor = true;
+            this.btnInstallLocation.Click += new System.EventHandler(this.btnInstallLocation_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 243);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 13);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Install Location:";
+            // 
+            // label14
+            // 
+            this.label14.Location = new System.Drawing.Point(14, 132);
+            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(427, 17);
+            this.label14.TabIndex = 17;
+            this.label14.Text = "This software under development is the Intellectual Property (IP) of GrainBound L" +
+    "LC";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(11, 110);
+            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(69, 13);
+            this.label13.TabIndex = 16;
+            this.label13.Text = "Disclaimer:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(172, 206);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(138, 13);
+            this.label8.TabIndex = 11;
+            this.label8.Text = "Bethlehem, PA, USA 18015";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(184, 183);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(126, 13);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "301 Broadway, Suite 500";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 206);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(140, 13);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "1-855-GRAINBD (472-4623)";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblEmail.Location = new System.Drawing.Point(14, 183);
+            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(111, 13);
+            this.lblEmail.TabIndex = 8;
+            this.lblEmail.Text = "info@grainbound.com";
+            this.lblEmail.Click += new System.EventHandler(this.lblEmail_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(11, 160);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(74, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Contact Us:";
             // 
             // label3
             // 
@@ -126,125 +241,22 @@ namespace GrainBound_Installer
             this.label3.TabIndex = 6;
             this.label3.Text = "Version: 1.1A";
             // 
-            // label4
+            // label2
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(9, 140);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(74, 13);
-            this.label4.TabIndex = 7;
-            this.label4.Text = "Contact Us:";
-            // 
-            // lblEmail
-            // 
-            this.lblEmail.AutoSize = true;
-            this.lblEmail.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.lblEmail.Location = new System.Drawing.Point(12, 163);
-            this.lblEmail.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(111, 13);
-            this.lblEmail.TabIndex = 8;
-            this.lblEmail.Text = "info@grainbound.com";
-            this.lblEmail.Click += new System.EventHandler(this.lblEmail_Click);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(11, 186);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(140, 13);
-            this.label6.TabIndex = 9;
-            this.label6.Text = "1-855-GRAINBD (472-4623)";
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(182, 163);
-            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(126, 13);
-            this.label7.TabIndex = 10;
-            this.label7.Text = "301 Broadway, Suite 500";
-            // 
-            // label8
-            // 
-            this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(170, 186);
-            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(138, 13);
-            this.label8.TabIndex = 11;
-            this.label8.Text = "Bethlehem, PA, USA 18015";
-            // 
-            // label13
-            // 
-            this.label13.AutoSize = true;
-            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label13.Location = new System.Drawing.Point(9, 90);
-            this.label13.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label13.Name = "label13";
-            this.label13.Size = new System.Drawing.Size(69, 13);
-            this.label13.TabIndex = 16;
-            this.label13.Text = "Disclaimer:";
-            // 
-            // label14
-            // 
-            this.label14.AutoSize = true;
-            this.label14.Location = new System.Drawing.Point(12, 112);
-            this.label14.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label14.Name = "label14";
-            this.label14.Size = new System.Drawing.Size(400, 13);
-            this.label14.TabIndex = 17;
-            this.label14.Text = "This software under development is the Intellectual Property (IP) of GrainBound L" +
-    "LC";
-            // 
-            // tboxLocation
-            // 
-            this.tboxLocation.Location = new System.Drawing.Point(12, 239);
-            this.tboxLocation.Name = "tboxLocation";
-            this.tboxLocation.Size = new System.Drawing.Size(448, 20);
-            this.tboxLocation.TabIndex = 18;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 223);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(81, 13);
-            this.label1.TabIndex = 19;
-            this.label1.Text = "Install Location:";
-            // 
-            // cboxDesktopShortcut
-            // 
-            this.cboxDesktopShortcut.AutoSize = true;
-            this.cboxDesktopShortcut.Checked = true;
-            this.cboxDesktopShortcut.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cboxDesktopShortcut.Location = new System.Drawing.Point(512, 241);
-            this.cboxDesktopShortcut.Name = "cboxDesktopShortcut";
-            this.cboxDesktopShortcut.Size = new System.Drawing.Size(143, 17);
-            this.cboxDesktopShortcut.TabIndex = 20;
-            this.cboxDesktopShortcut.Text = "Create Desktop Shortcut";
-            this.cboxDesktopShortcut.UseVisualStyleBackColor = true;
-            // 
-            // btnInstallLocation
-            // 
-            this.btnInstallLocation.Location = new System.Drawing.Point(464, 237);
-            this.btnInstallLocation.Name = "btnInstallLocation";
-            this.btnInstallLocation.Size = new System.Drawing.Size(42, 23);
-            this.btnInstallLocation.TabIndex = 21;
-            this.btnInstallLocation.Text = "...";
-            this.btnInstallLocation.UseVisualStyleBackColor = true;
-            this.btnInstallLocation.Click += new System.EventHandler(this.btnInstallLocation_Click);
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 9);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(296, 31);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Early Access Preview";
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 329);
+            this.ClientSize = new System.Drawing.Size(664, 345);
             this.Controls.Add(this.btnInstallLocation);
             this.Controls.Add(this.cboxDesktopShortcut);
             this.Controls.Add(this.label1);
@@ -284,19 +296,19 @@ namespace GrainBound_Installer
         private System.Windows.Forms.ProgressBar pgbProgress;
         private System.Windows.Forms.Label lblStatus;
         private System.Windows.Forms.PictureBox pboxLogo;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.Label label7;
-        private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Label label13;
-        private System.Windows.Forms.Label label14;
         private System.Windows.Forms.TextBox tboxLocation;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.CheckBox cboxDesktopShortcut;
         private System.Windows.Forms.Button btnInstallLocation;
+        private AALabel label2;
+        private AALabel label3;
+        private AALabel label4;
+        private AALabel lblEmail;
+        private AALabel label6;
+        private AALabel label7;
+        private AALabel label8;
+        private AALabel label13;
+        private AALabel label14;
+        private AALabel label1;
     }
 }
 
